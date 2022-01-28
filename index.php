201dd6e1967c51a39cca97403b24767b8d6f9967
Muhammad Subhan Raza <msraza302@gmail.com>
2022-01-28 21:00:32 +0500

    Project Created
    Index Page header implemented

---
+++ index.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Healing treasure | Home</title>
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+
+
+  <!-- ------------- INFO BAR ------------- -->
+
+  <div class="info-bar">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6">
+
+          <a href="#" class="info-bar-icons"><i class="fa fa-facebook"></i></a>
+          <a href="#" class="info-bar-icons"><i class="fa fa-twitter"></i></a>
+          <a href="#" class="info-bar-icons"><i class="fa fa-instagram"></i></a>
+          <a href="#" class="info-bar-icons"><i class="fa fa-linkedin"></i></a>
+
+        </div>
+        <div class="col-md-6 text-right">
+            <a href="#" class="info-bar-contanct"><i class="fa fa-phone"></i> +324 876786 6787</a>
+            <a href="#" class="info-bar-contanct"><i class="fa fa-envelope"></i> healing@gmial.com</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <!-- ----------------- NAVBAR ---------------- -->
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+
+
+      <nav class="navbar navbar-expand-lg">
+  <a class="navbar-brand" href="#">
+    <img src="./assets/images/logo.png" alt="">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <i class="fa fa-bars"></i>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Home</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Products</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">About us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Testimnials</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+      </div>
+    </div>
+  </div>
+
+
+
+<!-- ------------------ BANNER ------------------- -->
+
+
+    <header>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="header-inner">
+              <h1>Shop With Us.</h1>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem molestiae similique reprehenderit voluptates molestias deleniti ea voluptate. Quasi, quia est.</p>
+              <a href="#" class="btn-header-shop">Shop Now</a>
+              <a href="#" class="btn-header-read">Explore Now..</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+
+
+
+    <!-- ---------------------- SEC OUR PODUCTS -------------- -->
+
+
+    <section class="sec-our-product">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="our-products-sec-header">
+              <h6>POPULAR PRODUCTS</h6>
+              <h1>Our Products</h1>
+              <p>vptio officiis alias? Consequuntur eaque distinctio unde ullam? Provident fugiat perspiciatis accusamus, laborum cumque rem repudiandae culpa unde tempora?</p>
+            </div>
+          </div>
+        </div>
+
+
+        <div class="row">
+          <div class="col-md-4">
+            <div class="shop-card">
+              
+              <div class="card-image">
+              <div class="square"></div>
+                <img src="./assets/images/product1.png" alt="">
+              </div>
+              <div class="card-desrp">
+                <h6>Beautiful Gem</h6>
+                <span class="mr-5"><i class="fa fa-star"></i> 45</span>
+                <span><i class="fa fa-heart"></i> 77</span>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque, facilis?</p>
+                <a href="#" class="btn-card-cart">CART</a>
+                <a href="#" class="btn-card-view">VIEW</a>
+              </div>
+            </div>
+          </div>
+
+          
+        </div>
+
+      </div>
+    </section>
+
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+
+</body>
+</html>
